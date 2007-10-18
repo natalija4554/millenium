b41cfc5ae7f72226b6242e9134c5918ed49c58e9
fdurajka <fdurajka@3ab0620a-b93c-0410-8010-fd07a5101c97>
2007-10-18 00:09:43 +0000

    Share project 'Millenium' into 'https://millenium.googlecode.com/svn'

---
+++ trunk/application/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * IndexController - The default controller class
+ */
+
+require_once 'Zend/Controller/Action.php';
+
+class IndexController extends Zend_Controller_Action 
+{
+	/**
+	 * The default action - show the home page
+	 */
+    public function indexAction() 
+    {
+       // TODO 
+    }
+}
---
+++ trunk/html/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Millenium project
+ * 
+ * @author Frantisek Durajka
+ */
+set_include_path('.' . PATH_SEPARATOR . '../../library');
+require_once 'Zend/Controller/Front.php';
+
+/**
+ * Run the controller
+ */
+Zend_Controller_Front::run('../application/controllers');
