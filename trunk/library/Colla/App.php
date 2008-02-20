5989f9387db1ea874ab173896899456cd0484971
fdurajka <fdurajka@3ab0620a-b93c-0410-8010-fd07a5101c97>
2008-02-20 23:16:39 +0000

    svn keywords

--- trunk/library/Colla/App.php
+++ trunk/library/Colla/App.php
@@ -4,7 +4,7 @@
  *
  * @package Colla
  */
-class Colla_App
+final class Colla_App
 {
 	/**
 	 * Umiestnenie aplikacie
@@ -43,16 +43,91 @@ class Colla_App
         );
 	
 	/**
-	 * Implements singletron pattern
+	 * SingleTron pattern !
+	 * 
 	 * @return Colla_App
 	 */
 	public static function getInstance()
     {
-        if (null === self::$_instance) {
+        if (self::$_instance === null) {
             self::$_instance = new self();
             self::$_instance->_initialize();
         }
         return self::$_instance;
+    }
+    
+	/**
+     * Spusti aplikaciu
+     * 
+     */
+    public function run()
+    {
+    	// set options
+    	$front = Zend_Controller_Front::getInstance();
+    	$front->throwExceptions(false);
+    	$front->setBaseUrl($this->config->baseurl);
+    	$front->setControllerDirectory($this->dirApplication . DIRECTORY_SEPARATOR . 'controllers');
+    	$front->registerPlugin(new Colla_Controller_Plugin_Dispatch_Check());
+    	$front->registerPlugin(new Colla_Controller_Plugin_Auth());
+    	$front->registerPlugin(new Colla_Controller_Plugin_View_Layout());
+    	$front->returnResponse(true);    	
+    	
+    	// acl
+    	$acl = $this->getAcl();
+    	$acl->add(new Zend_Acl_Resource('index'))
+    		->add(new Zend_Acl_Resource('error'))
+            ->add(new Zend_Acl_Resource('login'))
+            ->add(new Zend_Acl_Resource('problemarea'))
+            ->add(new Zend_Acl_Resource('logout'))
+            ->add(new Zend_Acl_Resource('profile'))
+            ->addRole(new Zend_Acl_Role('anonymous'))
+            ->addRole(new Zend_Acl_Role('member'), 'anonymous')
+            ->addRole(new Zend_Acl_Role('admin'), 'member')
+            ->allow()
+            ->deny(null, 'problemarea')
+            ->allow('admin', 'problemarea');    	
+    	
+       	// go !
+    	//try {
+    		$response = $front->dispatch();
+    		$response->sendResponse();
+    	//} catch (Exception $e) {
+    		// echo 'Nastala chyba: ';
+    		// echo $e->getMessage();
+    		//print_r($e);
+ 	 	//}
+    }
+    
+	/**
+     * Initialize Application
+     * 
+     */
+	private function _initialize()
+    {
+    	// error reporting
+        error_reporting(E_ALL | E_STRICT);
+        
+		// check magic quotes
+		if (get_magic_quotes_gpc()) {
+			if (!empty($_GET)) $_GET = stripMagicQuotes($_GET);
+			if (!empty($_POST)) $_POST = stripMagicQuotes($_POST);
+			if (!empty($_REQUEST)) $_REQUEST = stripMagicQuotes($_REQUEST);
+			if (!empty($_COOKIE)) $_COOKIE = stripMagicQuotes($_COOKIE);
+		}
+                
+        // nadstav cesty
+        $this->dirLibrary 		= dirname(dirname(__FILE__));
+        $this->dirApplication 	= dirname($this->dirLibrary) . DIRECTORY_SEPARATOR . 'application';
+        
+        // inclde path
+        set_include_path($this->dirLibrary . PATH_SEPARATOR . $this->dirApplication . DIRECTORY_SEPARATOR . 'models' . PATH_SEPARATOR . get_include_path());
+		Zend_Loader::registerAutoload();
+ 
+        // read config file
+        $this->config = new Zend_Config_Xml($this->dirApplication . DIRECTORY_SEPARATOR . 'config.xml');
+        
+        // set default DB adapter
+        Zend_Db_Table_Abstract::setDefaultAdapter($this->getDb());
     }
     
     /**
@@ -82,83 +157,5 @@ class Colla_App
         }
         return $this->_services['db'];
     }
-	
-    /**
-     * Initialization
-     */
-	protected function _initialize()
-    {
-    	// error reporting
-        error_reporting(E_ALL | E_STRICT);
-        
-		// check magic quotes
-		if (get_magic_quotes_gpc()) {
-			if (!empty($_GET)) $_GET = stripMagicQuotes($_GET);
-			if (!empty($_POST)) $_POST = stripMagicQuotes($_POST);
-			if (!empty($_REQUEST)) $_REQUEST = stripMagicQuotes($_REQUEST);
-			if (!empty($_COOKIE)) $_COOKIE = stripMagicQuotes($_COOKIE);
-		}
-                
-        // nadstav cesty
-        $this->dirLibrary 		= dirname(dirname(__FILE__));
-        $this->dirApplication 	= dirname($this->dirLibrary) . DIRECTORY_SEPARATOR . 'application';
-        
-        // inclde path
-        set_include_path($this->dirLibrary . PATH_SEPARATOR . $this->dirApplication . DIRECTORY_SEPARATOR . 'models' . PATH_SEPARATOR . get_include_path());
-		Zend_Loader::registerAutoload();
- 
-        // read config file
-        $this->config = new Zend_Config_Xml($this->dirApplication . DIRECTORY_SEPARATOR . 'config.xml');
-        
-        // set default DB adapter
-        Zend_Db_Table_Abstract::setDefaultAdapter($this->getDb());
-    }
-    
-    /**
-     * Spusti aplikaciu
-     * 
-     */
-    public function run()
-    {
-    	// set options
-    	$front = Zend_Controller_Front::getInstance();
-    	$front->throwExceptions($this->config->render_exceptions ? true : false);
-    	$front->setBaseUrl($this->config->baseurl);
-    	$front->setControllerDirectory($this->dirApplication . DIRECTORY_SEPARATOR . 'controllers');
-    	$front->registerPlugin(new Colla_Controller_Plugin_Dispatch_Check());
-    	$front->registerPlugin(new Colla_Controller_Plugin_Auth());
-    	$front->registerPlugin(new Colla_Controller_Plugin_View_Layout());
-    	$front->returnResponse(true);    	
-    	
-    	// acl
-    	$acl = $this->getAcl();
-    	$acl->add(new Zend_Acl_Resource('index'))
-    		->add(new Zend_Acl_Resource('error'))
-            ->add(new Zend_Acl_Resource('login'))
-            ->add(new Zend_Acl_Resource('problemarea'))
-            ->add(new Zend_Acl_Resource('logout'))
-            ->add(new Zend_Acl_Resource('profile'))
-            ->addRole(new Zend_Acl_Role('anonymous'))
-            ->addRole(new Zend_Acl_Role('member'), 'anonymous')
-            ->addRole(new Zend_Acl_Role('admin'), 'member')
-            ->allow()
-            ->deny(null, 'problemarea')
-            ->allow('admin', 'problemarea');    	
-    	
-       	// go !
-    	try {
-    		$response = $front->dispatch();
-    		$response->sendResponse();
-    	} catch (Exception $e) {
-    		echo 'Nastala chyba: ';
-    		echo $e->getMessage();
- 	 	}
-    }
-    
-    private function __construct()
-    {}
-    private function __clone()
-    {}
 }
-
 ?>
--- trunk/public/index.php
+++ trunk/public/index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Colla Application
+ * 
+ * @category Colla
+ * @package Colla_Application
+ * @version $Id$
+ */
 // require Zend Loader & Application
 require_once '../library/Zend/Loader.php';
 require_once '../library/Colla/App.php';
