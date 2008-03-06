75b84deddfe0b2f346b622a080eddfa9eb9d889d
fdurajka <fdurajka@3ab0620a-b93c-0410-8010-fd07a5101c97>
2008-03-06 15:06:28 +0000

    - multilanguage support

--- library/Colla/Controller/Action.php
+++ library/Colla/Controller/Action.php
@@ -5,24 +5,5 @@
  */
 class Colla_Controller_Action extends Zend_Controller_Action
 {
-    public function preDispatch()
-    {
-    	// ziska objekt view
-        $view = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->view;
-
-        // nastavi premenne
-        // - authenticated
-        // - user
-        $auth = Zend_Auth::getInstance();
-        if ($view->authenticated = $auth->hasIdentity()) {
-            $view->user = new Colla_Model_User($auth->getIdentity());
-        } else {
-            $view->user = new Colla_Model_User();
-        }
-   	}
-
-    public function __call($name, $args)
-    {
-        throw new Exception('Sorry, the requested action does not exist');
-    }
+    
 }
--- library/Colla/Db/Table/ProblemArea.php
+++ library/Colla/Db/Table/ProblemArea.php
@@ -1,5 +1,5 @@
 <?php
 class Colla_Db_Table_ProblemArea extends Zend_Db_Table_Abstract
 {
-    protected $_name = 'problem_area';
+    protected $_name = 'ProblemAreas';
 }
