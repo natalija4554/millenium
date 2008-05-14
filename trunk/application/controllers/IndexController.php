14b399dd2f4ab06bdb77c327d19200d9c7f0da59
fdurajka <fdurajka@3ab0620a-b93c-0410-8010-fd07a5101c97>
2008-05-14 09:54:45 +0000

    zmeneny index forward

--- trunk/application/controllers/IndexController.php
+++ trunk/application/controllers/IndexController.php
@@ -4,6 +4,7 @@ class IndexController extends Colla_Controller_Action
 {
  	public function indexAction()
     {
+    	$this->_forward('problems', 'problemarea');
     }
 
     public function deniedAction()
